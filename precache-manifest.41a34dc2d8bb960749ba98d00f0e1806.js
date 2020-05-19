9194f905900dd68716e6a115c7f7fc1a99a926ff
precious123-gifted <ogunameprecious@gmail.com>
2020-05-19 02:09:48 +0100

    Updates

---
+++ precache-manifest.41a34dc2d8bb960749ba98d00f0e1806.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cab1de2fa5275fa14a50c4173917b9",
+    "url": "/luixie/index.html"
+  },
+  {
+    "revision": "df80387e053597575fc7",
+    "url": "/luixie/static/css/main.090ad382.chunk.css"
+  },
+  {
+    "revision": "1bcbbff7d2305fed6378",
+    "url": "/luixie/static/js/2.4fefc3bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/luixie/static/js/2.4fefc3bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df80387e053597575fc7",
+    "url": "/luixie/static/js/main.87c55cfa.chunk.js"
+  },
+  {
+    "revision": "84538dfb3a3de3b888ae",
+    "url": "/luixie/static/js/runtime-main.3e094f2c.js"
+  },
+  {
+    "revision": "9bd25fa08ef43716fb08eabdb9306fe9",
+    "url": "/luixie/static/media/luixieslogo.9bd25fa0.png"
+  },
+  {
+    "revision": "73149cfd5a23d97c395b490db0ef00aa",
+    "url": "/luixie/static/media/slide1.73149cfd.jpg"
+  },
+  {
+    "revision": "bee36bb775304128ae2e20fcb30e4772",
+    "url": "/luixie/static/media/slide2.bee36bb7.jpg"
+  },
+  {
+    "revision": "35dd0f2edce30fab8c55c484426f8b1a",
+    "url": "/luixie/static/media/slide3.35dd0f2e.jpg"
+  },
+  {
+    "revision": "b031e9d11ddc615802353a18344efd56",
+    "url": "/luixie/static/media/slide4.b031e9d1.jpg"
+  },
+  {
+    "revision": "1d8fa9bd840df9d382406ab933219922",
+    "url": "/luixie/static/media/slide5.1d8fa9bd.jpg"
+  },
+  {
+    "revision": "7dfd94f60a798493c76e038a96d52212",
+    "url": "/luixie/static/media/slide6.7dfd94f6.jpg"
+  },
+  {
+    "revision": "0ece2349107b1332a49c0eb52a359e47",
+    "url": "/luixie/static/media/slide7.0ece2349.jpg"
+  },
+  {
+    "revision": "b43e5722fafeb1a0f528cfe8a2266cdc",
+    "url": "/luixie/static/media/why.b43e5722.jpg"
+  }
+]);
